4755abf1a3516ba29e5c970423268d89f4d632e9
MiguelC <miguel.castama@gmail.com>
2023-10-21 19:09:33 -0600

    Añadir comentarios a file.sh

--- 02_ingest/raw_download.sh
+++ 02_ingest/raw_download.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#export YEAR=${YEAR:=2015}
+# Constantes
 SOURCE=https://transtats.bts.gov/PREZIP
-
 OUTDIR=raw
+
+# Crear o reemplazar directorios
 mkdir -p $OUTDIR
 
-for YEAR in `seq 2019 2019`; do
-for MONTH in `seq 1 12`; do
-
-  FILE=On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${YEAR}_${MONTH}.zip
-  curl -k -o ${OUTDIR}/${FILE}  ${SOURCE}/${FILE}
-
-done
+for YEAR in $(seq 2019 2019);
+do
+    for MONTH in $(seq 1 12);
+    do
+        # Armar URL de descarga
+        FILE=On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${YEAR}_${MONTH}.zip
+        # GET UTR y almacenar
+        curl -k -o ${OUTDIR}/${FILE} ${SOURCE}/${FILE}
+    done
 done
