a95292da7be5dba3d1de054e97256206f39129d1
MiguelC <miguel.castama@gmail.com>
2023-10-21 20:56:36 -0600

    Renombrar archivo y comentarios

---
+++ 02_ingest/3_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Mensaje de uso en caso de faltar argumentos
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./upload.sh  [BUCKET]"
+    exit
+fi
+
+# Asignar argumentos a variables
+BUCKET=$1
+
+# Cargar contenido a bucket de Cloud Storage
+echo "Uploading to bucket $BUCKET..."
+gsutil -m cp *.csv gs://$BUCKET/flights/raw/
